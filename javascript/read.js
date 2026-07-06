938679f4d88b8349fdc1cc5dd7f93fbb4d170aeb
grzegorz.grablewski <ggrablewski@wp.pl>
2026-07-06 17:34:55 +0200

    javascript: move to a folder, add small amendments (directory, filenames)

---
+++ javascript/read.js
@@ -0,0 +1,43 @@
+import {mkdir, writeFile} from 'fs/promises';
+import path from 'path';
+
+const posts = [];
+const filenames = [];
+
+const currentPath = path.basename(process.cwd());
+
+function setBaseDir(currentPath) {
+    switch (currentPath) {
+        case "javascript":
+            return "posts_js";
+        case "data-from-api":
+            return path.join("javascript", "posts_js");
+        default:
+            return path.join("data-from-api", "javascript", "posts_js");
+    }
+}
+
+const baseDir = setBaseDir(currentPath);
+
+await mkdir(baseDir, {recursive: true});
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => posts.push(post));
+        console.log('All posts using fetch:');
+        posts.forEach(post => {
+            filenames[post.id] = path.join(
+                baseDir,
+                `post_${String(post.id).padStart(3, "0")}.json`
+            );
+            console.log(`Filename: <${filenames[post.id]}>\n`, post);
+            writeFile(filenames[post.id], JSON.stringify(post))
+                .then(() => {
+                    console.log(`File <${filenames[post.id]}> saved successfully.`);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
+    });
