5ae20bd6ffb4d5ad6c6c3fc368d5f4159de36b3f
Grzegorz Grablewski <ggrablewski@wp.pl>
2026-07-14 22:44:38 +0200

    js: added the split betweem downloading posts and saving files

--- javascript/read.js
+++ javascript/read.js
@@ -21,23 +21,25 @@ const baseDir = setBaseDir(currentPath);
 
 await mkdir(baseDir, {recursive: true});
 
-fetch('https://jsonplaceholder.typicode.com/posts')
+await fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {
         data.forEach(post => posts.push(post));
-        console.log('All posts using fetch:');
-        posts.forEach(post => {
-            filenames[post.id] = path.join(
-                baseDir,
-                `post_${String(post.id).padStart(3, "0")}.json`
-            );
-            console.log(`Filename: <${filenames[post.id]}>\n`, post);
-            writeFile(filenames[post.id], JSON.stringify(post))
-                .then(() => {
-                    console.log(`File <${filenames[post.id]}> saved successfully.`);
-                })
-                .catch(err => {
-                    console.error(err);
-                });
-        });
+        console.log('All posts using fetch:', posts);
     });
+
+posts.forEach(post => {
+    filenames[post.id] = path.join(
+        baseDir,
+        `post_${String(post.id).padStart(3, "0")}.json`
+    );
+    console.log(`Filename: <${filenames[post.id]}>\n`, post);
+    writeFile(filenames[post.id], JSON.stringify(post))
+        .then(() => {
+            console.log(`File <${filenames[post.id]}> saved successfully.`);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
